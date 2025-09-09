6a358c0ef597ef0608ed6e980e298fff38b2f307
errmakov <ide404@gmail.com>
2025-09-09 15:08:23 +0200

    brew flags fix

--- scripts/10_brew_bundle.sh
+++ scripts/10_brew_bundle.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 eval "$($(uname -m | grep -q arm64 && echo /opt/homebrew/bin/brew || echo /usr/local/bin/brew) shellenv)"
 brew update
-brew bundle --file="$(git rev-parse --show-toplevel)/Brewfile" --no-lock
+brew bundle --file="$(git rev-parse --show-toplevel)/Brewfile"
 
